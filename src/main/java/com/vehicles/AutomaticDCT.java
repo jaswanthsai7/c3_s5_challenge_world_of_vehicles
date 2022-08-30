fce73e1f5e32eedeba0924f970d0aa520d9bce59
Jaswanth Sai <jasvanthsai7@gmail.com>
2022-08-30 20:04:22 +0530

    created constructor methods in AutomaticDCT.java class

--- src/main/java/com/vehicles/AutomaticDCT.java
+++ src/main/java/com/vehicles/AutomaticDCT.java
@@ -8,4 +8,13 @@ package com.vehicles;
 public class AutomaticDCT extends Transmission {
     private double[] allGearRatios;
     private int forwardGears;
+
+    public AutomaticDCT() {
+    }
+
+    public AutomaticDCT(String modelNumber) {
+        super(modelNumber);
+        this.allGearRatios = new double[]{4.714, 3.143, 2.106, 1.667, 1.285, 1.000, 0.839, 0.667};
+        this.forwardGears = allGearRatios.length;
+    }
 }
