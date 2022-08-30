b426cf1eff241cb964670c294af3e8380fe6cebb
Jaswanth Sai <jasvanthsai7@gmail.com>
2022-08-30 19:52:04 +0530

    created constructor method in AutomaticCVT.java class

--- src/main/java/com/vehicles/AutomaticCVT.java
+++ src/main/java/com/vehicles/AutomaticCVT.java
@@ -8,4 +8,13 @@ package com.vehicles;
 public class AutomaticCVT extends Transmission {
     private double[] allGearRatios;
     private int forwardGears;
+
+    public AutomaticCVT() {
+    }
+
+    public AutomaticCVT(String modelNumber, double[] allGearRatios, int forwardGears) {
+        super(modelNumber);
+        this.allGearRatios = new double[]{2.631, 1.440, 1.165, 0.960, 0.680, 0.499};
+        this.forwardGears = 6;
+    }
 }
