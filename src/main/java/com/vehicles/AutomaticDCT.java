8e69141c76feb269545ec4d5dcbc531a3ba55d32
Jaswanth Sai <jasvanthsai7@gmail.com>
2022-08-30 20:20:53 +0530

    created showSpecs method in AutomaticDCT.java class

--- src/main/java/com/vehicles/AutomaticDCT.java
+++ src/main/java/com/vehicles/AutomaticDCT.java
@@ -17,4 +17,14 @@ public class AutomaticDCT extends Transmission {
         this.allGearRatios = new double[]{4.714, 3.143, 2.106, 1.667, 1.285, 1.000, 0.839, 0.667};
         this.forwardGears = allGearRatios.length;
     }
+
+    public void showSpecs() {
+        System.out.println("Transmission Name : AutomaticDCT");
+        System.out.println("Transmission Model Number : " + getModelNumber());
+        System.out.println("1) Forward Gears " + forwardGears);
+        for (int i = 0; i < allGearRatios.length; i++) {
+            System.out.println((i + 2) + ") " + (i + 1) + "Gear Ratio " + allGearRatios[i] + "0");
+        }
+
+    }
 }
