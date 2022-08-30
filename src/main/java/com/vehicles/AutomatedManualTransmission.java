4b6139c3f47aed13ba1b3f2b53df0a5ed36329e9
Jaswanth Sai <jasvanthsai7@gmail.com>
2022-08-30 19:45:43 +0530

    created constructor methods in AutomatedManualTransmission.java class

--- src/main/java/com/vehicles/AutomatedManualTransmission.java
+++ src/main/java/com/vehicles/AutomatedManualTransmission.java
@@ -9,5 +9,18 @@ public class AutomatedManualTransmission extends Transmission {
     private double[] allGearRatios;
     private int frontGears;
 
+    public AutomatedManualTransmission() {
+    }
 
+    public AutomatedManualTransmission(String modelNumber) {
+        super(modelNumber);
+        if (modelNumber.equals("AMTP4")) {
+            this.frontGears = 4;
+            this.allGearRatios = new double[]{2.540, 1.920, 1510, 1.000};
+        } else if (modelNumber.equals("AMTD5")) {
+            this.frontGears = 5;
+            this.allGearRatios = new double[]{2.950, 1.940, 1.340, 1.000, 0.630};
+        }
+
+    }
 }
