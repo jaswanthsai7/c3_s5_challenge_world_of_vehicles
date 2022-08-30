50604c3503e20b593c9adb9367ee11fe1c492549
Jaswanth Sai <jasvanthsai7@gmail.com>
2022-08-30 19:49:10 +0530

    changed the name of the field in AutomatedManualTransmission

--- src/main/java/com/vehicles/AutomatedManualTransmission.java
+++ src/main/java/com/vehicles/AutomatedManualTransmission.java
@@ -7,7 +7,7 @@ package com.vehicles;
 
 public class AutomatedManualTransmission extends Transmission {
     private double[] allGearRatios;
-    private int frontGears;
+    private int forwardGears;
 
     public AutomatedManualTransmission() {
     }
@@ -15,10 +15,10 @@ public class AutomatedManualTransmission extends Transmission {
     public AutomatedManualTransmission(String modelNumber) {
         super(modelNumber);
         if (modelNumber.equals("AMTP4")) {
-            this.frontGears = 4;
+            this.forwardGears = 4;
             this.allGearRatios = new double[]{2.540, 1.920, 1510, 1.000};
         } else if (modelNumber.equals("AMTD5")) {
-            this.frontGears = 5;
+            this.forwardGears = 5;
             this.allGearRatios = new double[]{2.950, 1.940, 1.340, 1.000, 0.630};
         }
 
@@ -29,14 +29,14 @@ public class AutomatedManualTransmission extends Transmission {
         if (allGearRatios.length == 4) {
             System.out.println("Transmission Name : AutomatedManualTransmission");
             System.out.println("Transmission Model Number : AMTP4");
-            System.out.println("1) Forward Gears " + frontGears);
+            System.out.println("1) Forward Gears " + forwardGears);
             for (int i = 0; i < allGearRatios.length; i++) {
                 System.out.println((i + 2) + ")" + " Gear Ratio " + allGearRatios[i] + "0");
             }
         } else if (allGearRatios.length == 5) {
             System.out.println("Transmission Name : AutomatedManualTransmission");
             System.out.println("Transmission Model Number : AMTD5");
-            System.out.println("1) Forward Gears " + frontGears);
+            System.out.println("1) Forward Gears " + forwardGears);
             for (int i = 0; i < allGearRatios.length; i++) {
                 System.out.println((i + 2) + ")" + " Gear Ratio " + (allGearRatios[i]) + "0");
 
